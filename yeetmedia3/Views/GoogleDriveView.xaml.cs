e7d4fcfe9272d8ba4f5215e44b492764c78973b8
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-09-23 21:56:45 -0500

    Fix edit json on Android

--- yeetmedia3/Views/GoogleDriveView.xaml.cs
+++ yeetmedia3/Views/GoogleDriveView.xaml.cs
@@ -1,4 +1,5 @@
 using Yeetmedia3.ViewModels;
+using DriveFile = Google.Apis.Drive.v3.Data.File;
 
 namespace Yeetmedia3.Views;
 
@@ -26,6 +27,25 @@ public partial class GoogleDriveView : ContentPage
         catch (Exception ex)
         {
             await DisplayAlert("Error", $"Failed to create new JSON file: {ex.Message}", "OK");
+        }
+    }
+
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection?.FirstOrDefault() is DriveFile selectedFile)
+        {
+            System.Diagnostics.Debug.WriteLine($"[GoogleDriveView] File selected: {selectedFile.Name}");
+            _viewModel.SelectedFile = selectedFile;
+        }
+    }
+
+    private void OnItemTapped(object sender, EventArgs e)
+    {
+        if (sender is Border border && border.BindingContext is DriveFile file)
+        {
+            System.Diagnostics.Debug.WriteLine($"[GoogleDriveView] Item tapped: {file.Name}");
+            // Manually set the selected item when tapped
+            _viewModel.SelectedFile = file;
         }
     }
 }
